58eabc13bb600067d14cd2ca8bc30e7fbfddadfc
Alex A <alex.av936@gmail.com>
2020-05-10 22:05:01 -0500

    first class in swift

--- PlayWithSwift.playground/Contents.swift
+++ PlayWithSwift.playground/Contents.swift
@@ -1,6 +1,22 @@
 import UIKit
 
-var str = "Hello, playground"
+class Person{
+    var name: String
+    var age: Int
 
-print(str)
-print("Hello, world!")
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    
+    func greeting(){
+        print("Your name is \(self.name), you are \(self.age) years old")
+    }
+}
+
+var person1 = Person(name: "Alex", age: 27)
+
+person1.age
+person1.name
+
+person1.greeting()
